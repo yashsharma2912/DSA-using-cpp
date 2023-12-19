f291156ffea7e33162c035ebed7b72bd2ccabfc4
Om Khulbe <84437272+yashsharma2912@users.noreply.github.com>
2023-12-19 09:36:11 +0530

    Update best_time_to_buy_and_sell_stock.cpp

--- best_time_to_buy_and_sell_stock.cpp
+++ best_time_to_buy_and_sell_stock.cpp
@@ -1,8 +1,11 @@
 class Solution {
 public:
 	int maxProfit(vector<int>& prices) {
+	 // initially profit will be 0
 	 int maxi = 0;
+	 // minimum profit should be stay in constrain range which is 10 power 4
 	 int mini = 100000;
+	 // we declared n to get access of an array index
 	 int n = prices.size();
 	   for(int i =0;i<n;i++){
 		   mini = min(mini,prices[i]);
